c5034d7b3306665a8571f96ee52ee7323f1e8529
Ivan Niello <ivaniello@hotmail.com>
2022-06-05 16:02:41 -0300

    entrega con promise

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,30 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import {ItemList} from './ItemList'
+import React, { useEffect, useState } from "react";
+import { ItemList } from "./ItemList";
 
-export default function ItemListContainer ({greeting, user, catalogo}) {
-  
-useEffect(() =>{
-  const task = new Promise((res, rej)=> {
+export default function ItemListContainer({ greeting, user, catalogo }) {
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+  const [resultado, setResultado] = useState();
 
-    setTimeout(() => {
-      res('cumplida')
-    }, 2000);
-      
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    setResultado();
+
+    const task = new Promise((res, rej) => {
+      setTimeout(() => {
+        res({
+          mensaje: "usted compro ",
+          name: "Remera Blanca Athlos",
+          precio: 1300,
+        });
+        rej("hubo un error");
+      }, 2000);
     });
-    
-    console.log(task)
-  
+
+    task
+      .then((result) => {
+        setResultado(result);
+      })
+      .catch((error) => {
+        setError(true);
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
- 
-return (
-      <>
+  return (
+    <>
       <br />
-    <div>{greeting + " " + user + " " + catalogo}
-    <br />
-    <ItemList products/>
-    </div>
+      <div>
+        {greeting + " " + user + " " + catalogo}
+        <br />
+        <ItemList />
+      </div>
+      <br />
+      <br />
+      <div>{loading && "Loading.."}</div>
+      <div>{error && "Hubo un error inesperado"}</div>
+      <div>
+        {resultado && (
+          <>
+            {resultado.mensaje}
+            {resultado.name}
+            {resultado.precio}
+          </>
+        )}
+      </div>
     </>
-  )
+  );
 }
-
