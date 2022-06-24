c334546a4c5aafde38262e26232783373e158037
Ivan Niello <ivaniello@hotmail.com>
2022-06-23 22:30:24 -0300

    render

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from '../context/CartContext';
+
 
 export default function Cart() {
+  const { carrito, quitarCarrito, agregarCarrito, enCarrito, precioTotal, cantidadTotal } = useContext(CartContext);
+
   return (
-    <div>Este es el Carrito</div>
+<>
+{carrito.length > 0 ? (
+  <div>
+    <div>
+      <div>
+      <h3>Item</h3>
+      <h3>Categoria</h3>
+      <h3>Precio</h3>
+      <h3>Cantidad</h3>
+      </div>
+    </div>
+  </div>
+): (
+  <CarritoVacio/>
+  )}
+</>
   )
 }
+//hacer un coso de CarritoVacio 
